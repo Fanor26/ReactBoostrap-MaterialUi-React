01bb12b9d59c064710dcb18201f937506b98b5ea
Fanoro <fanorstalin@gmail.com>
2022-07-30 20:45:12 +0000

    organizate components add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './App.css';
-import  { Buttons } from './Buttons'
+import  { Buttons } from './components/buttons/Buttons'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import { Autocompletes } from './components/autocompletes/Autocompletes';
 function App() {
   return (
-    <div className="App">
-   <Buttons />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/autocomplete" element={<Autocompletes />} />
+        <Route path="/button" element={<Buttons />} />
+        </Routes>
+    </Router>
   );
 }
 
